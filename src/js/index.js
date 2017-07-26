4b93f0a08dc996df27e091d9340b1ef812e3f2dc
Joshua Ringer <joshringer@users.noreply.github.com>
2017-07-26 16:55:26 -0400

    add volume control

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 import $ from 'jquery'
 
+const masterVolDnKeyCode = 173
+const masterVolUpKeyCode = 61
+
 var ctx = new (window.AudioContext || window.webkitAudioContext)()
 var masterVolume = ctx.createGain()
-
-masterVolume.gain.value = 0.3
-masterVolume.connect(ctx.destination)
 
 function setAudioParam (param, value) {
   // console.log('setAudioParam', param, value)
@@ -16,9 +16,16 @@ function setAudioParam (param, value) {
 }
 
 function createOscillator (el) {
-  // instantiate nodes and bind to el
-  var osc = $(el).data('osc') || ctx.createOscillator()
+  // instantiate nodes
+  var osc = $(el).data('osc')
+  var toStart = true
+  if (osc) {
+    toStart = false
+  } else {
+    osc = ctx.createOscillator()
+  }
   var vol = $(el).data('vol') || ctx.createGain()
+  // attach to el data
   $(el).data({ osc: osc, vol: vol })
   // set initial values
   updateOscillator(el, 'type', $(el).find('.type').text())
@@ -28,13 +35,13 @@ function createOscillator (el) {
   // connect to audio pipeline and start oscillator
   osc.connect(vol)
   vol.connect(masterVolume)
-  osc.start()
+  if (toStart) { osc.start() }
 }
 
 function updateOscillator (el, property, value) {
   var node = $(el).data((property === 'gain') ? 'vol' : 'osc')
   var newValue
-  console.log('update', node, property, node[property], 'to', value)
+  // console.log('update', node, property, node[property], 'to', value)
   if (node[property] instanceof window.AudioParam) {
     newValue = setAudioParam(node[property], value)
   } else {
@@ -51,13 +58,43 @@ function destroyOscillator (el) {
   $(el).removeData(['osc', 'vol'])
 }
 
+// check for volume change & initialise
+$('#mastervolume').change(function (ev) {
+  // console.log('mastervolume change', ev.target)
+  var volume = parseInt($(ev.target).val())
+  setAudioParam(masterVolume.gain, volume / 10)
+  $('#mastervolumedisplay [data-level]').each(function (_, el) {
+    if (volume < parseInt($(el).data('level'))) {
+      $(el).removeClass('on')
+    } else {
+      $(el).addClass('on')
+    }
+  })
+}).change()
+masterVolume.connect(ctx.destination)
+
+// check for key presses
 $(window).keydown(function (ev) {
-  // console.log('down', ev.which)
-  $('.key[data-keycode=' + ev.which + ']').each(function (_, el) {
+  // console.log('keydown', ev.which)
+  $('[data-type=oscillator][data-keycode=' + ev.which + ']').each(function (_, el) {
     createOscillator(el)
   })
+  if (ev.which === masterVolDnKeyCode) {
+    $('#mastervolume').val(function (_, v) {
+      v = parseInt(v)
+      return v > 0 ? v - 1 : 0
+    }).change()
+  }
+  if (ev.which === masterVolUpKeyCode) {
+    $('#mastervolume').val(function (_, v) {
+      v = parseInt(v)
+      return v < 11 ? v + 1 : 11
+    }).change()
+  }
+  $('[data-keycode=' + ev.which + ']').addClass('pressed')
 }).keyup(function (ev) {
-  $('.key[data-keycode=' + ev.which + ']').each(function (_, el) {
+  $('[data-keycode=' + ev.which + ']').removeClass('pressed')
+  $('[data-type=oscillator][data-keycode=' + ev.which + ']').each(function (_, el) {
     destroyOscillator(el)
   })
 })
