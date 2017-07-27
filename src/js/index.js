6e8440ac0c86743670f1b6f6537c9479d994a58a
Joshua Ringer <joshringer@users.noreply.github.com>
2017-07-27 16:40:34 -0400

    Add oscillator config + custom waveform

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ var masterVolume = ctx.createGain()
 
 function setAudioParam (param, value) {
   // console.log('setAudioParam', param, value)
+
   if (isNaN(value)) value = param.defaultValue
   else if (value < param.minValue) value = param.minValue
   else if (value > param.maxValue) value = param.maxValue
@@ -16,14 +17,10 @@ function setAudioParam (param, value) {
 }
 
 function createOscillator (el) {
-  // instantiate nodes
+  // create nodes
   var osc = $(el).data('osc')
-  var toStart = true
-  if (osc) {
-    toStart = false
-  } else {
-    osc = ctx.createOscillator()
-  }
+  var toStart = !osc
+  osc = osc || ctx.createOscillator()
   var vol = $(el).data('vol') || ctx.createGain()
   // attach to el data
   $(el).data({ osc: osc, vol: vol })
@@ -35,20 +32,29 @@ function createOscillator (el) {
   // connect to audio pipeline and start oscillator
   osc.connect(vol)
   vol.connect(masterVolume)
-  if (toStart) { osc.start() }
+  // if already created, we do not start again
+  if (toStart) osc.start()
 }
 
 function updateOscillator (el, property, value) {
   var node = $(el).data((property === 'gain') ? 'vol' : 'osc')
-  var newValue
   // console.log('update', node, property, node[property], 'to', value)
-  if (node[property] instanceof window.AudioParam) {
-    newValue = setAudioParam(node[property], value)
-  } else {
-    node[property] = value
-    newValue = node[property]
+
+  // check for node since we are creating on demand
+  if (node) {
+    if (node[property] instanceof window.AudioParam) {
+      value = setAudioParam(node[property], value)
+    } else {
+      if (property === 'type' && value === 'custom') {
+        // we are hoping customWave data has been created by this point
+        node.setPeriodicWave($(el).data('customWave'))
+      } else {
+        node[property] = value
+      }
+      value = node[property]
+    }
   }
-  $(el).find('.' + property).text(newValue)
+  $(el).find('.' + property).text(value)
 }
 
 function destroyOscillator (el) {
@@ -58,13 +64,28 @@ function destroyOscillator (el) {
   $(el).removeData(['osc', 'vol'])
 }
 
+function createCustomWave (paramsel) {
+  var real = new Float32Array(
+    $(paramsel).find('[data-param=real]').map(function (_, el) {
+      return $(el).val()
+    }).get()
+  )
+  var imag = new Float32Array(
+    $(paramsel).find('[data-param=imag]').map(function (_, el) {
+      return $(el).val()
+    }).get()
+  )
+  return ctx.createPeriodicWave(real, imag)
+}
+
 // check for volume change & initialise
 $('#mastervolume').change(function (ev) {
   // console.log('mastervolume change', ev.target)
+
   var volume = parseInt($(ev.target).val())
   setAudioParam(masterVolume.gain, volume / 10)
   $('#mastervolumedisplay [data-level]').each(function (_, el) {
-    if (volume < parseInt($(el).data('level'))) {
+    if (volume < $(el).data('level')) {
       $(el).removeClass('on')
     } else {
       $(el).addClass('on')
@@ -73,12 +94,32 @@ $('#mastervolume').change(function (ev) {
 }).change()
 masterVolume.connect(ctx.destination)
 
+$('#oscillatortype,#customoscillator input').change(function () {
+  var type = $('#oscillatortype').val()
+  if (type === 'custom') {
+    $('#customoscillator input').prop('disabled', false)
+    var wave = createCustomWave('#customoscillator')
+    $('[data-type=oscillator]').each(function (_, el) {
+      $(el).data('customWave', wave)
+      updateOscillator(el, 'type', 'custom')
+    })
+  } else {
+    $('#customoscillator input').prop('disabled', true)
+    $('[data-type=oscillator]').each(function (_, el) {
+      updateOscillator(el, 'type', type)
+    })
+  }
+}).change()
+
 // check for key presses
 $(window).keydown(function (ev) {
   // console.log('keydown', ev.which)
+
+  // process oscillators
   $('[data-type=oscillator][data-keycode=' + ev.which + ']').each(function (_, el) {
     createOscillator(el)
   })
+  // process master volume
   if (ev.which === masterVolDnKeyCode) {
     $('#mastervolume').val(function (_, v) {
       v = parseInt(v)
@@ -91,9 +132,12 @@ $(window).keydown(function (ev) {
       return v < 11 ? v + 1 : 11
     }).change()
   }
+  // add key press class
   $('[data-keycode=' + ev.which + ']').addClass('pressed')
 }).keyup(function (ev) {
+  // remove key press class
   $('[data-keycode=' + ev.which + ']').removeClass('pressed')
+  // process oscillators
   $('[data-type=oscillator][data-keycode=' + ev.which + ']').each(function (_, el) {
     destroyOscillator(el)
   })
