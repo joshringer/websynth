ac65bffbe4a30bc327e1f7775776128da821bc35
Joshua Ringer <joshringer@users.noreply.github.com>
2017-09-13 12:49:42 +0800

    some more safety checks to publish script

--- publish.sh
+++ publish.sh
@@ -2,14 +2,22 @@
 #
 # This script publishes current build to gh-pages branch
 #
+set -e
 
 cd "$(dirname $0)"
 
 # check for changes and abort
-if ! git diff-index --quiet HEAD --
+if ! git diff-index --quiet HEAD
 then
   echo "Current checkout has changes. Please commit first."
   exit 1
+fi
+
+# check current commits are pushed
+if test -n "$(git rev-list @{u}..HEAD)"
+then
+  echo "Current checkout is ahead of remote. Please push first."
+  exit 2
 fi
 
 # perform fresh build
