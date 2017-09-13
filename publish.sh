c5f5887f11a22fcf7815176210be71a6e9e7aaea
Joshua Ringer <joshringer@users.noreply.github.com>
2017-09-13 12:27:06 +0800

    Add publish script to push dist to gh-pages branch

---
+++ publish.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#
+# This script publishes current build to gh-pages branch
+#
+
+cd "$(dirname $0)"
+
+# check for changes and abort
+if ! git diff-index --quiet HEAD --
+then
+  echo "Current checkout has changes. Please commit first."
+  exit 1
+fi
+
+# perform fresh build
+yarn run build
+
+repo="$(git remote get-url origin)"
+if test -z "$1"
+then
+  message="Update to revision $(git rev-parse --short HEAD)"
+else
+  message="$1"
+fi
+
+# create gh-pages subdir
+test -d .gh-pages || git clone $repo .gh-pages
+# update
+cd .gh-pages
+git checkout --quiet gh-pages
+git pull origin gh-pages
+
+# remove existing files
+rm -rf *
+# copy in current dist files
+cp -a ../dist/* .
+
+# commit and push
+git add .
+git commit -m "$message"
+git push origin gh-pages
