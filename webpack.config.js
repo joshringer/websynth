83f08e19d0a6f79704ba95d454b640ddfa935a00
Joshua Ringer <joshringer@users.noreply.github.com>
2017-09-13 09:04:59 +0800

    use html webpack plugin to generate html, place output in dist/

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
+const path = require('path')
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   entry: './src/js/index.js',
   output: {
+    path: path.join(__dirname, 'dist'),
     filename: 'app.js'
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'WebAudio Experiments',
+      template: 'src/ejs/index.ejs'
+    })
+  ]
 }
